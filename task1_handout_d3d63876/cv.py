011d050067a152c04c64a58711027c7c787d02c6
mezhang111 <91820741+mezhang111@users.noreply.github.com>
2021-10-06 18:18:54 +0200

    cv for parameter searching

---
+++ task1_handout_d3d63876/cv.py
@@ -0,0 +1,225 @@
+import os
+import typing
+
+from sklearn.gaussian_process.kernels import *
+import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import scipy
+import sklearn.kernel_approximation
+from tqdm import tqdm
+
+# Set `EXTENDED_EVALUATION` to `True` in order to visualize your predictions.
+EXTENDED_EVALUATION = False
+EVALUATION_GRID_POINTS = 300  # Number of grid points used in extended evaluation
+EVALUATION_GRID_POINTS_3D = 50  # Number of points displayed in 3D during evaluation
+
+
+# Cost function constants
+THRESHOLD = 35.5
+COST_W_NORMAL = 1.0
+COST_W_OVERPREDICT = 5.0
+COST_W_THRESHOLD = 20.0
+
+def decision_fun(gp_mean, gp_std):
+    res = np.empty(shape = gp_mean.shape)
+    for i in range(len(gp_mean)):
+        p = 1 - scipy.stats.norm.cdf(x = THRESHOLD, loc = gp_mean[i], scale = gp_std[i])
+        curr = gp_mean[i] - 0.2*gp_std[i]
+        if curr < THRESHOLD and p > 0.3:
+            res[i] = THRESHOLD
+        else:
+            res[i] = curr
+    return res
+
+class Model(object):
+    """
+    Model for this task.
+    You need to implement the fit_model and predict methods
+    without changing their signatures, but are allowed to create additional methods.
+    """
+
+    def __init__(self, c, h):
+        """
+        Initialize your model here.
+        We already provide a random number generator for reproducibility.
+        """
+        self.rng = np.random.default_rng(seed=0)
+        self.kernel = ConstantKernel(c, constant_value_bounds = 'fixed')*RBF(h, length_scale_bounds = 'fixed')
+        self.gp = GaussianProcessRegressor(kernel = self.kernel, random_state = 0, optimizer = 'fmin_l_bfgs_b') #use default kernel
+        # TODO: Add custom initialization for your model here if necessary
+
+    def predict(self, x: np.ndarray) -> typing.Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Predict the pollution concentration for a given set of locations.
+        :param x: Locations as a 2d NumPy float array of shape (NUM_SAMPLES, 2)
+        :return:
+            Tuple of three 1d NumPy float arrays, each of shape (NUM_SAMPLES,),
+            containing your predictions, the GP posterior mean, and the GP posterior stddev (in that order)
+        """
+
+        # TODO: Use your GP to estimate the posterior mean and stddev for each location here
+        gp_mean, gp_std = self.gp.predict(x, return_std = True)
+        
+
+        # TODO: Use the GP posterior to form your predictions here
+
+        predictions = decision_fun(gp_mean, gp_std)
+
+        return predictions, gp_mean, gp_std
+
+    def fit_model(self, train_x: np.ndarray, train_y: np.ndarray):
+        """
+        Fit your model on the given training data.
+        :param train_x: Training features as a 2d NumPy float array of shape (NUM_SAMPLES, 2)
+        :param train_y: Training pollution concentrations as a 1d NumPy float array of shape (NUM_SAMPLES,)
+        """
+        #feature_map_nystroem = sklearn.kernel_approximation.Nystroem(kernel = 'linear', n_jobs = -1) #approximate with nystroem
+        self.gp.fit(train_x, train_y)
+        # TODO: Fit your model here
+        pass
+
+
+def cost_function(y_true: np.ndarray, y_predicted: np.ndarray) -> float:
+    """
+    Calculates the cost of a set of predictions.
+
+    :param y_true: Ground truth pollution levels as a 1d NumPy float array
+    :param y_predicted: Predicted pollution levels as a 1d NumPy float array
+    :return: Total cost of all predictions as a single float
+    """
+    assert y_true.ndim == 1 and y_predicted.ndim == 1 and y_true.shape == y_predicted.shape
+
+    # Unweighted cost
+    cost = (y_true - y_predicted) ** 2
+    weights = np.zeros_like(cost)
+
+    # Case i): overprediction
+    mask_1 = y_predicted > y_true
+    weights[mask_1] = COST_W_OVERPREDICT
+
+    # Case ii): true is above threshold, prediction below
+    mask_2 = (y_true >= THRESHOLD) & (y_predicted < THRESHOLD)
+    weights[mask_2] = COST_W_THRESHOLD
+
+    # Case iii): everything else
+    mask_3 = ~(mask_1 | mask_2)
+    weights[mask_3] = COST_W_NORMAL
+
+    # Weigh the cost and return the average
+    return np.mean(cost * weights)
+
+
+def perform_extended_evaluation(model: Model, output_dir: str = '/results'):
+    """
+    Visualizes the predictions of a fitted model.
+    :param model: Fitted model to be visualized
+    :param output_dir: Directory in which the visualizations will be stored
+    """
+    print('Performing extended evaluation')
+    fig = plt.figure(figsize=(30, 10))
+    fig.suptitle('Extended visualization of task 1')
+
+    # Visualize on a uniform grid over the entire coordinate system
+    grid_lat, grid_lon = np.meshgrid(
+        np.linspace(0, EVALUATION_GRID_POINTS - 1, num=EVALUATION_GRID_POINTS) / EVALUATION_GRID_POINTS,
+        np.linspace(0, EVALUATION_GRID_POINTS - 1, num=EVALUATION_GRID_POINTS) / EVALUATION_GRID_POINTS,
+    )
+    visualization_xs = np.stack((grid_lon.flatten(), grid_lat.flatten()), axis=1)
+
+    # Obtain predictions, means, and stddevs over the entire map
+    predictions, gp_mean, gp_stddev = model.predict(visualization_xs)
+    predictions = np.reshape(predictions, (EVALUATION_GRID_POINTS, EVALUATION_GRID_POINTS))
+    gp_mean = np.reshape(gp_mean, (EVALUATION_GRID_POINTS, EVALUATION_GRID_POINTS))
+    gp_stddev = np.reshape(gp_stddev, (EVALUATION_GRID_POINTS, EVALUATION_GRID_POINTS))
+
+    vmin, vmax = 0.0, 65.0
+    vmax_stddev = 35.5
+
+    # Plot the actual predictions
+    ax_predictions = fig.add_subplot(1, 3, 1)
+    predictions_plot = ax_predictions.imshow(predictions, vmin=vmin, vmax=vmax)
+    ax_predictions.set_title('Predictions')
+    fig.colorbar(predictions_plot)
+
+    # Plot the raw GP predictions with their stddeviations
+    ax_gp = fig.add_subplot(1, 3, 2, projection='3d')
+    ax_gp.plot_surface(
+        X=grid_lon,
+        Y=grid_lat,
+        Z=gp_mean,
+        facecolors=cm.get_cmap()(gp_stddev / vmax_stddev),
+        rcount=EVALUATION_GRID_POINTS_3D,
+        ccount=EVALUATION_GRID_POINTS_3D,
+        linewidth=0,
+        antialiased=False
+    )
+    ax_gp.set_zlim(vmin, vmax)
+    ax_gp.set_title('GP means, colors are GP stddev')
+
+    # Plot the standard deviations
+    ax_stddev = fig.add_subplot(1, 3, 3)
+    stddev_plot = ax_stddev.imshow(gp_stddev, vmin=vmin, vmax=vmax_stddev)
+    ax_stddev.set_title('GP estimated stddev')
+    fig.colorbar(stddev_plot)
+
+    # Save figure to pdf
+    figure_path = os.path.join(output_dir, 'extended_evaluation.pdf')
+    fig.savefig(figure_path)
+    print(f'Saved extended evaluation to {figure_path}')
+
+    plt.show()
+
+def cv(parameters, x, y):
+    k = 5
+    kf = KFold(n_splits = k)
+    errors = np.empty(shape = len(parameters))
+    i = 0
+    for (c,h) in tqdm(parameters):
+        error = 0
+        for train_index, test_index in kf.split(x):
+            x_train, x_test = x[train_index], x[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            model = Model(c = c, h = h)
+            model.fit_model(x_train, y_train)
+            y_predicted, y_mean, y_std = model.predict(x_test)
+            error += cost_function(y_test, y_predicted)
+        errors[i] = error/k
+        i += 1
+    index = np.argmin(errors)
+    print(errors[index])
+    print(parameters[index])
+    return parameters[index]
+
+
+def main():
+    # Load the training dateset and test features
+    train_x = np.loadtxt('train_x.csv', delimiter=',', skiprows=1)
+    train_y = np.loadtxt('train_y.csv', delimiter=',', skiprows=1)
+    test_x = np.loadtxt('test_x.csv', delimiter=',', skiprows=1)
+
+    # Fit the model
+    print('Fitting model')
+
+    #model = Model()
+    #model.fit_model(train_x, train_y)
+
+    # Predict on the test features
+    #print('Predicting on test features')
+    #predicted_y = model.predict(test_x)
+    #print(predicted_y)
+
+    c = [2**x for x in range(3,7)] #first round in [2^-1, 2^4]
+    h = [2**x for x in range(-6, -3)] #first round in [2^-5, 2^1]
+    parameters = [(a,b) for a in c for b in h]
+    param = cv(parameters, train_x, train_y)
+
+    if EXTENDED_EVALUATION:
+        perform_extended_evaluation(model, output_dir='.')
+
+
+if __name__ == "__main__":
+    main()
